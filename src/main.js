7e957730d1771eab1b2e4293ae2b2d5ffe714d68
Jahyeon Choi <jahyeon.choi@gmail.com>
2022-08-28 20:01:06 +0900

    Add Router

--- src/main.js
+++ src/main.js
@@ -1,31 +1,47 @@
 // @ts-check
 
 const express = require('express')
-const fs = require('fs')
+// const bodyParser = require('body-parser')
 
 const app = express()
+const userRouter = express.Router()
+
+app.use(express.json())
+app.use('/users', userRouter)
+
+const Users = {
+  15: {
+    nickname: 'foo',
+  },
+}
 
 const PORT = 5000
+userRouter.get('/', (req, res) => {
+  res.send('User list')
+})
 
-app.use('/', async (req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.log('Middleware1')
-
-  const fileContent = await fs.promises.readFile('.gitignore')
-
+userRouter.param('id', (req, res, next, value) => {
   // @ts-ignore
-  req.requestedAt = new Date()
-  // @ts-ignore
-  req.fileContent = fileContent
+  req.user = Users[value]
   next()
 })
 
-app.use((req, res) => {
-  // eslint-disable-next-line no-console
-  console.log('Middleware2')
-
+userRouter.get('/:id', (req, res) => {
   // @ts-ignore
-  res.send(`Hello, Express! ${req.requestedAt}, ${req.fileContent}`)
+  res.send(req.user)
+})
+
+userRouter.post('/', (req, res) => {
+  res.send('User Registered.')
+})
+
+userRouter.post('/:id/nickname', (req, res) => {
+  // @ts-ignore
+  const { user } = req
+  const { nickname } = req.body
+  user.nickanme = nickname
+
+  res.send('User nickname Updated')
 })
 
 app.listen(PORT, () => {
