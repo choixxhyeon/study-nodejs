8d392c423aa085cdb4a59c9706ecc53485004be1
Jahyeon Choi <jahyeon.choi@gmail.com>
2022-08-28 19:21:39 +0900

    Add middleware

--- src/main.js
+++ src/main.js
@@ -1,13 +1,31 @@
 // @ts-check
 
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 
 const PORT = 5000
 
-app.use('/', (req, res) => {
-  res.send('Hello, Express!')
+app.use('/', async (req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.log('Middleware1')
+
+  const fileContent = await fs.promises.readFile('.gitignore')
+
+  // @ts-ignore
+  req.requestedAt = new Date()
+  // @ts-ignore
+  req.fileContent = fileContent
+  next()
+})
+
+app.use((req, res) => {
+  // eslint-disable-next-line no-console
+  console.log('Middleware2')
+
+  // @ts-ignore
+  res.send(`Hello, Express! ${req.requestedAt}, ${req.fileContent}`)
 })
 
 app.listen(PORT, () => {
