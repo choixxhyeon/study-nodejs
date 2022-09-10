b2707cb9ee4f18a2958795f2980e9447bdb72b26
Jahyeon Choi <jahyeon.choi@gmail.com>
2022-09-10 22:22:11 +0900

    Add bugs command

--- src/github-cli/main.js
+++ src/github-cli/main.js
@@ -1,5 +1,6 @@
 // @ts-check
 const { program } = require('commander')
+const { url } = require('inspector')
 const { Octokit } = require('octokit')
 
 require('dotenv').config()
@@ -9,12 +10,39 @@ const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN })
 
 program
   .command('me')
-  .description('Check my profile')
+  .description('Check my profile.')
   .action(async () => {
     const {
       data: { login },
     } = await octokit.rest.users.getAuthenticated()
+
     console.log('Hello, %s!', login)
+  })
+
+program
+  .command('bugs')
+  .description('Issues with bug label.')
+  .action(async () => {
+    const result = await octokit.rest.issues.listForRepo({
+      owner: 'choixxhyeon',
+      repo: 'study-nodejs',
+    })
+
+    console.log(
+      'Bugs : ',
+      result.data
+        .filter(
+          (issue) =>
+            // @ts-ignore
+            issue.labels.find((label) => label.name === 'bug') !== undefined
+        )
+        .map((issue) => ({
+          url: issue.url,
+          title: issue.title,
+          content: issue.body,
+          createdBy: issue.user?.login,
+        }))
+    )
   })
 
 program.parseAsync()
